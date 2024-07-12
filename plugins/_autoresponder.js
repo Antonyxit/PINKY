6086c2f215379cb75af313e6cffb515adad8d5e6
Antonyxit <carlosmorga45@gmail.com>
2024-07-11 18:55:51 -0600

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝑴𝒐𝒏𝒌𝒆𝒚 𝑩𝒐𝒕 𝘾𝙧𝙚𝙖𝙙𝙤 𝙥𝙤𝙧 𝙈𝙤𝙣𝙤𝙫𝙖𝙣 𝙑𝙚𝙣𝙩𝙖𝙨', body: '𝙈𝙤𝙣𝙤𝙫𝙖𝙣 𝙑𝙚𝙣𝙩𝙖𝙨', sourceUrl: md, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: '𝑫𝑨𝑵𝑵𝒀𝑩𝑶𝑻 𝘾𝙧𝙚𝙖𝙙𝙤 𝙥𝙤𝙧 𝑫𝑨𝑵𝑵𝒀 𝙑𝙚𝙣𝙩𝙖𝙨', body: '𝑫𝑨𝑵𝑵𝒀 𝙑𝙚𝙣𝙩𝙖𝙨', sourceUrl: md, thumbnail: imagen2}}})}
     
 return !0 }
 export default handler
